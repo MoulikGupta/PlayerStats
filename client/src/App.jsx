19590db8aa95e9d4367496e7aafeb89fa34707fc
MoulikGupta <moulikgupta8@gmail.com>
2025-03-09 00:43:16 +0530

    another fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -14,7 +14,7 @@ function App() {
 
   const getPlayers = async()=>{
     try {
-      const response = await fetch("https://playerstats.onrender.com//players")
+      const response = await fetch("https://playerstats.onrender.com/players")
       const jsonData = await response.json()
       setPlayers(jsonData)
     } catch (err) {
