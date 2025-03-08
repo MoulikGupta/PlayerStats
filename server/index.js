e530ccae66edf0cb7f5988d6593986ef47726073
MoulikGupta <moulikgupta8@gmail.com>
2025-03-09 01:48:44 +0530

    i

--- server/index.js
+++ server/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const cors = require("cors");
 const app = express();
 const pool = require("./db");
-
+const path = require('path');
 app.use(cors());
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'dist')));  
